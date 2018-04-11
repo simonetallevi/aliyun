7f54907b51ca24bbf4e9515e88c09e93b5b1728e
Simone Tallevi-Diotallevi <simone.tallevi@pirelli.com>
2018-04-11 12:54:29 +0200

    adding lambda function on alicloud

---
+++ function-compute-example/src/main/java/com/simone/aliyun/fc/Sample.java
@@ -0,0 +1,18 @@
+package com.simone.aliyun.fc;
+
+import com.aliyun.fc.runtime.Context;
+import com.aliyun.fc.runtime.StreamRequestHandler;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+
+public class Sample implements StreamRequestHandler {
+
+    @Override
+    public void handleRequest(
+            InputStream inputStream, OutputStream outputStream, Context context) throws IOException {
+        outputStream.write(new String("hello world").getBytes());
+    }
+}
